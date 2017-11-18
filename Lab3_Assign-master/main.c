56d38eb5767a445c3ed9326c225a5cad144ae913
rcallaham <rjc2751@vt.edu>
2017-11-17 19:15:53 -0500

    Update main.c

--- Lab3_Assign-master/main.c
+++ Lab3_Assign-master/main.c
@@ -1,5 +1,4 @@
 /* DriverLib Includes */
-#/* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 /*
  * NEW FUNCTIONS or STRUCTS USED IN THE LAB3 KEY:
@@ -34,15 +33,25 @@
 #include <stdbool.h>
 #include "printf.h"
 
-#define MAX_DUTY_CYCLE 255;
-
-#define RED     255;
-#define GREEN   255;
-#define BLUE    255;
-
 static volatile uint8_t readdata;
-volatile int debounced = 0;
-//bool timerRunning = false;
+
+//Have we debounced the switch? 0 if no, 1 if yes.
+volatile uint8_t debounced = 0;
+
+//Is the stopwatch running? 1 if yes, 0 if no.
+volatile uint8_t timerRunning = 1;
+
+//Char array used to store the 8bit color values for the RGB LED
+volatile char nums[3];
+
+//Tracks the length of nums[]
+volatile uint8_t numsLen = 0;
+
+//Have we received a r, g, or b recently? 0 if false, 1 if true.
+volatile uint8_t rgbState = 0;
+
+//Which LED should we modify? R, G, or B?
+volatile uint32_t color = 0;
 
 // TODO: Lab3: create UART, Timer_A, and CCR config structures ~~~~~~~~~~
 // UART config
@@ -61,31 +70,31 @@ EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
 const Timer_A_UpModeConfig A0_PWM = {
 TIMER_A_CLOCKSOURCE_SMCLK, // Usually DCO clock, which in this case we set to 12MHz in main()
         TIMER_A_CLOCKSOURCE_DIVIDER_1, 255, //based on example slides. equal to CCR0
-        TIMER_A_TAIE_INTERRUPT_ENABLE, // Should Timer_A send interrupts to Processor *at all*
-        TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE, // Should Timer_A reaching its period value (stored in CCR0) trigger an interrupt?
+        TIMER_A_TAIE_INTERRUPT_DISABLE, // Should Timer_A send interrupts to Processor *at all*
+        TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE, // Should Timer_A reaching its period value (stored in CCR0) trigger an interrupt?
         TIMER_A_DO_CLEAR };
 
 // All 3 CCR configs for the 3 pins of the RGB LED
 
 //CCR1
 
-const Timer_A_CompareModeConfig A0_CCR0 = {
+const Timer_A_CompareModeConfig a0_CCR1 = {
         TIMER_A_CAPTURECOMPARE_REGISTER_1,
-        TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE,
-        TIMER_A_OUTPUTMODE_OUTBITVALUE, 255 };
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,
+        TIMER_A_OUTPUTMODE_SET_RESET, 0 };
 
 //CR2
-const Timer_A_CompareModeConfig A0_CCR1 = {
+const Timer_A_CompareModeConfig a0_CCR2 = {
         TIMER_A_CAPTURECOMPARE_REGISTER_2,
-        TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE,
-        TIMER_A_OUTPUTMODE_OUTBITVALUE, 255     //25% duty cycle
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,
+        TIMER_A_OUTPUTMODE_SET_RESET, 0     //25% duty cycle
         };
 
 //CCR3
-const Timer_A_CompareModeConfig A0_CCR2 = {
+const Timer_A_CompareModeConfig a0_CCR3 = {
         TIMER_A_CAPTURECOMPARE_REGISTER_3,
-        TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE,
-        TIMER_A_OUTPUTMODE_OUTBITVALUE, 255 //75% duty cycle
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,
+        TIMER_A_OUTPUTMODE_SET_RESET, 0 //75% duty cycle
         };
 
 // PROVIDED: Timer_A struct for our debounce timer, triggering its interrupt when it hits its period value in CCR0
@@ -119,7 +128,10 @@ int main(void)
             GPIO_PRIMARY_MODULE_FUNCTION);
 
     /* TODO: LAB3 Set DCO to 12MHz */
-    CS_setDCOFrequency(CS_DCO_FREQUENCY_12);
+    CS_setDCOFrequency(12000000);
+
+    //YEAH,WE DO NEED THIS BRENDYN ugh
+    SysCtl_enableSRAMBankRetention(SYSCTL_SRAM_BANK1);
 
 //    Generic Interrupt enabling:
     MAP_Interrupt_enableSleepOnIsrExit();
@@ -137,19 +149,19 @@ int main(void)
 //           Configure Timer_A2 for debouncing, using its interrupt to prevent it from cycling forever.
 //            (Struct for Timer A2 is provided, such that when it hits its CCR0 an interrupt is triggered
 //          Start with the stopwatch paused, so it only starts actually counting in response to the relevant UART commands
-    //Timer32
+
+    //Timer32 initialization and interrupt enabling
     MAP_Timer32_initModule(TIMER32_0_BASE, TIMER32_PRESCALER_1, TIMER32_32BIT,
     TIMER32_PERIODIC_MODE);
+    MAP_Timer32_setCount(TIMER32_0_BASE, UINT32_MAX);
     MAP_Timer32_clearInterruptFlag(TIMER32_0_BASE);
     MAP_Timer32_enableInterrupt(TIMER32_0_BASE);
     MAP_Interrupt_enableInterrupt(TIMER32_0_INTERRUPT);
+    MAP_Timer32_startTimer(TIMER32_0_BASE, 0);
 
-    //enable interrupts on GPIO port 1, pin 4
+    //Enable interrupts on GPIO port 1, pin 4
     MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1,
     GPIO_PIN4);
-
-    //SysCtl_enableSRAMBankRetention(SYSCTL_SRAM_BANK1);
-
     MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN4);
     MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN4);
     MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P1, GPIO_PIN4,
@@ -163,6 +175,10 @@ int main(void)
 
     //        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ENTER CODE FOR LAB3 stopwatch init HERE:
 
+    //Configure timer A2 and enable interrupt
+    MAP_Timer_A_configureUpMode(TIMER_A2_BASE, &debounce_Config);
+    MAP_Interrupt_enableInterrupt(INT_TA2_0);
+
 //        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ End CODE FOR LAB3 stopwatch init
 
 //          TODO: LAB3, Part 2: Prepare RGB LED driven by PWM signals driven by TIMER_A0 with multiple CCRs. Note that because the processor
@@ -170,11 +186,14 @@ int main(void)
 //          We can initially drive them with a 100% duty cycle for testing; the UART commands can easily change the duty cycle on their own
 //        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ENTER CODE FOR LAB3 RC_RGB init HERE:
     // Initialize CCRS
-    MAP_Timer_A_initCompare(TIMER_A0_BASE, &A0_CCR0);
+    MAP_Timer_A_configureUpMode(TIMER_A0_BASE, &A0_PWM);
+    MAP_Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
 
-    MAP_Timer_A_initCompare(TIMER_A0_BASE, &A0_CCR1);
+    MAP_Timer_A_initCompare(TIMER_A0_BASE, &a0_CCR1);
 
-    MAP_Timer_A_initCompare(TIMER_A0_BASE, &A0_CCR2);
+    MAP_Timer_A_initCompare(TIMER_A0_BASE, &a0_CCR2);
+
+    MAP_Timer_A_initCompare(TIMER_A0_BASE, &a0_CCR3);
 
 //        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ End CODE FOR LAB3 RC_RGB init
 
@@ -195,22 +214,23 @@ int main(void)
 // TODO: Lab3: Resumes, Pauses, or Restarts stopwatch depending on restart flag and the stopwatch state
 static void startStopStopwatch(bool restart)
 {
-//    if (restart) //if ! is called, restart the timer
-//    {
-//        Timer32_haltTimer(TIMER32_0_BASE); //stops timer
-//        Timer32_setCount(TIMER32_0_BASE, UINT32_MAX); //resets timer
-//        timerRunning = 1;      //resets timer running variable
-//    }
-//    else if (timerRunning)    // is timer running
-//    {
-//        Timer32_haltTimer(TIMER32_0_BASE);   //stop timer
-//    }
-//    else //timer is not running
-//    {
-//        Timer32_startTimer(TIMER32_0_BASE, 0); //start timer
-//    }
-//
-//    timerRunning = !timerRunning; //toggle timer running state
+    if (restart) //if ! is called, restart the timer
+    {
+        MAP_Timer32_setCount(TIMER32_0_BASE, 0); //resets timer
+        timerRunning = 1;      //resets timer running variable
+    }
+
+    if (timerRunning == 1)    // is timer running
+    {
+        MAP_Timer32_haltTimer(TIMER32_0_BASE);   //stop timer
+        timerRunning = 0;
+    }
+    else //timer is not running
+    {
+        MAP_Timer32_startTimer(TIMER32_0_BASE, 0); //start timer
+        timerRunning = 1;
+    }
+    //timerRunning = !timerRunning; //toggle timer running state
 }
 // END Stopwatch State Helper Function Definition ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -218,9 +238,9 @@ static void startStopStopwatch(bool restart)
 // TODO: Lab3: Sample current stopwatch value, convert to number of elapsed milliseconds, and transmit to computer
 void stopwatchSample()
 {
-    uint32_t watchTime = Timer32_getValue(TIMER32_0_BASE);
-    MAP_UART_transmitData(EUSCI_A0_BASE,
-                          (INT32_MAX - watchTime) / CS_getDCOFrequency());
+    uint32_t watchTime = ((UINT32_MAX - Timer32_getValue(TIMER32_0_BASE))
+            / 12000);
+    printf(EUSCI_A0_BASE, "Current Time: %n milliseconds\r\n", watchTime);
 }
 // END Sample Stopwatch code ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -231,7 +251,7 @@ uint16_t atoi(volatile char* s, uint8_t l)
     uint8_t i;
     for (i = 0; i < l; i++)
     {
-        r = 10 * r + (s[i] - '0');
+        r = 10 * r + (s[i] - 48);
     }
     return r;
 }
@@ -244,98 +264,113 @@ extern void EUSCIA0_IRQHandler(void)
 //    also tracks parsing state (most recently seen letter, and numeric characters in current number) to understand
 //          the relevant multicharacter commands, as per spec
 
-    uint32_t status = UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
+    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
 
     if (status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
     {
         readdata = UART_receiveData(EUSCI_A0_BASE);
+        MAP_UART_clearInterruptFlag(EUSCI_A0_BASE, status);
 
-        // Toggle Red LED if the character received is an "L":
-//        if (readdata == 76)
-//        {
-//            MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
-//
-//            const char mytext[] = "Toggle LED\n\r";
-//            int ichar;
-//            for (ichar = 0; ichar < 12; ichar++)
-//            {
-//                MAP_UART_transmitData(EUSCI_A0_BASE, mytext[ichar]);
-//            }
-//        }
-
-        //rgb
-        if (readdata == 'r')
+        //Handles the receipt of characters after an r, g, or b
+        if (rgbState == 1)
         {
-            uint16_t red_duty;
-            volatile char* nums;
-            uint8_t i = 0;
-            volatile uint8_t curr = MAP_UART_receiveData(EUSCI_A0_BASE);
-            while (curr > 47 && curr < 58)
+            //adds numeric characters to the nums array
+            if (readdata >= '0' && readdata <= '9')
             {
-                nums += curr;
-                i++;
-                curr = MAP_UART_receiveData(EUSCI_A0_BASE);
+                nums[numsLen] = readdata;
+                numsLen++;
+                return;
             }
-            red_duty = atoi(nums, i);
+            //after receiving a non numeric char, uses atoi to compute an int from values in
+            //nums, clear nums, reset numsLen, resets rgb state, and changes the duty cycle
+            //based on the color.
+            else
+            {
+                uint8_t duty = atoi(nums, numsLen);
 
-            MAP_Timer_A_setCompareValue(TIMER_A0_BASE,
-            TIMER_A_CAPTURECOMPARE_REGISTER_1,
-                                        red_duty);
+                nums[0] = 0;
+                nums[1] = 0;
+                nums[2] = 0;
+
+                numsLen = 0;
+
+                rgbState = 0;
+
+                switch (color)
+                {
+                case 'r':
+                {
+                    MAP_Timer_A_setCompareValue(TIMER_A0_BASE,
+                    TIMER_A_CAPTURECOMPARE_REGISTER_1,
+                                                duty);
+
+                    break;
+                }
+                case 'g':
+                {
+                    MAP_Timer_A_setCompareValue(TIMER_A0_BASE,
+                    TIMER_A_CAPTURECOMPARE_REGISTER_2,
+                                                duty);
+                    break;
+                }
+                case 'b':
+                {
+                    MAP_Timer_A_setCompareValue(TIMER_A0_BASE,
+                    TIMER_A_CAPTURECOMPARE_REGISTER_3,
+                                                duty);
+                    break;
+                }
+                default:
+                {
+                    return;
+                }
+
+                }
+            }
         }
-        else if (readdata == 'g')
+
+        //r
+        if (readdata == 114)
         {
-            uint8_t green_duty;
-            volatile char nums[3];
-            int i = 0;
-            volatile char curr = UART_receiveData(EUSCI_A0_BASE);
-            while (curr > 47 && curr < 58)
-            {
-                nums[i] = curr;
-                i++;
-            }
-            green_duty = atoi(nums, i);
-            Timer_A_setCompareValue(TIMER_A0_BASE,
-            TIMER_A_CAPTURECOMPARE_REGISTER_2,
-                                    green_duty);
+            rgbState = 1;
+            color = 'r';
+            return;
         }
-        else if (readdata == 'b')
+        //g
+        else if (readdata == 103)
         {
-            uint8_t blue_duty;
-            volatile char nums[3];
-            int i = 0;
-            volatile char curr = UART_receiveData(EUSCI_A0_BASE);
-            while (curr > 47 && curr < 58)
-            {
-                nums[i] = curr;
-                i++;
-            }
-            blue_duty = atoi(nums, i);
-            Timer_A_setCompareValue(TIMER_A0_BASE,
-            TIMER_A_CAPTURECOMPARE_REGISTER_3,
-                                    blue_duty);
+            rgbState = 1;
+            color = 'g';
+            return;
+        }
+        //b
+        else if (readdata == 98)
+        {
+            rgbState = 1;
+            color = 'b';
+            return;
         }
         //s
-        else if (readdata == 's')
+        else if (readdata == 115)
         {
             startStopStopwatch(false);
         }
         //!
-        else if (readdata == '!')
+        else if (readdata == 33)
         {
             startStopStopwatch(true);
         }
         //p
-        else if (readdata == 'p')
+        else if (readdata == 112)
         {
             stopwatchSample();
         }
-        //other
+        //echo undesired character
         else
         {
             MAP_UART_transmitData(EUSCI_A0_BASE, readdata);
             MAP_UART_transmitData(EUSCI_A0_BASE, '.');
         }
-        MAP_UART_clearInterruptFlag(EUSCI_A0_BASE, status);
 
     }
 }
@@ -344,7 +379,7 @@ extern void EUSCIA0_IRQHandler(void)
 // TODO: Lab3: Button Interrupt, Debounce Interrupt, and Stopwatch Interrupt ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 extern void PORT1_IRQHandler()
 {
-//debouncing logic
+    //debouncing logic
     if (debounced == 1)
     {
         MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN4);
@@ -354,11 +389,12 @@ extern void PORT1_IRQHandler()
     debounced = 1;
     MAP_Timer_A_startCounter(TIMER_A2_BASE, TIMER_A_UP_MODE);
 
-//get pin
+    //get pin
     uint32_t status;
     status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P1);
     MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN4);
 
+    // sample stopwatch
     if (status & GPIO_PIN4)
     {
         stopwatchSample();
@@ -379,6 +415,9 @@ extern void TA2_0_IRQHandler()
 extern void T32_INT0_IRQHandler()
 {
 //      Stopwatch Interrupt: Complains about stopwatch count running out in the far, far future
+    MAP_Timer32_setCount(TIMER32_0_BASE, UINT32_MAX);
+    MAP_Timer32_startTimer(TIMER32_0_BASE, 0);
+
 }
 
 // END LAB3 BUTTON AND STOPWATCH INTERRUPTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
